4e1da56edf5551abb6d378ba4209dc33ddeb4c3a
Aaron Farrelly <X00203931@mytudublin.ie>
2023-12-01 01:42:43 +0000

    Added profile page view, stylized CSS more & added a new logo

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from shop import views 
-from .views import SignUpView, CustomPasswordChangeView
+from .views import SignUpView, CustomPasswordChangeView, ProfilePageView,ProfileUpdateView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('profile/<int:pk>/', ProfilePageView.as_view(), name='profile_view'),
+    path('profile_edit/<int:pk>/', ProfileUpdateView.as_view(), name='profile_edit'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth.models import Group
 from .forms import CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Profile
 from django.contrib.auth.views import PasswordChangeView, LogoutView
 
 class SignUpView(CreateView):
@@ -17,6 +17,19 @@ class SignUpView(CreateView):
             return redirect('login')
         else:
             return render(request, self.template_name, {'form' : form })
+        
+class ProfilePageView(DetailView):
+    model = Profile
+    template_name = 'profile.html'
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    template_name = 'profile_edit.html'
+    fields = ['dob']
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
         
 class CustomPasswordChangeView(PasswordChangeView):
     template_name = 'registration/password_change_form.html'
