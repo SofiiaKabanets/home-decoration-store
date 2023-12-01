af1b59fe6e4d2c51e0f7b77be9b955caeacbbedf
Sofia Kabanets <X00217101@mytudublin.ie>
2023-12-01 19:33:40 +0000

    search app

--- decorationproject/urls.py
+++ decorationproject/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('search/', include('search_app.urls')),
     path('',include('shop.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
